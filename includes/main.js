ae6512a31afce43b58bd14f08ee37342c1ff7248
zhiwutian <brettalbright@outlook.com>
2019-10-31 17:15:43 -0700

    completed iniital refactor of main.js file with new js-docs

--- includes/main.js
+++ includes/main.js
@@ -25,6 +25,7 @@ function initiateApp(){
 	*/
 	makeGallery(pictures);
 	addModalCloseHandler();
+	$('#gallery').sortable();
 }
 function makeGallery(imageArray){
 	//use loops and jquery dom creation to make the html structure inside the #gallery section
@@ -32,20 +33,37 @@ function makeGallery(imageArray){
 	//create a loop to go through the images in the imageArray
 		//create the elements needed for each picture, store the elements in variable
 
-		//attach a click handler to the figure you create.  call the "displayImage" function.  
+		//attach a click handler to the figure you create.  call the "displayImage" function.
 
 		//append the element to the #gallery section
-	
-	// side note: make sure to remove the hard coded html in the index.html when you are done!
 
+	// side note: make sure to remove the hard coded html in the index.html when you are done!
+		for(var imageIndex = 0; imageIndex<imageArray.length;imageIndex++){
+			var domImageConfig = {
+				class: "imageGallery col-xs-12 col-sm-6 col-md-4",
+				style: "background-image:url("+imageArray[imageIndex]+");",
+				imgData: imageArray[imageIndex],
+				click: displayImage,
+
+			}
+			var domImage = $('<figure>',domImageConfig);
+			var domImageCaption = $('<figcaption>',{text:imageArray[imageIndex].slice(7)});
+
+			domImage.append(domImageCaption);
+			$('#gallery').append(domImage);
+		}
 }
 
 function addModalCloseHandler(){
 	//add a click handler to the img element in the image modal.  When the element is clicked, close the modal
-	//for more info, check here: https://www.w3schools.com/bootstrap/bootstrap_ref_js_modal.asp	
+	//for more info, check here: https://www.w3schools.com/bootstrap/bootstrap_ref_js_modal.asp
+	$('.modal-body > img').click(function(){$('#galleryModal').modal('hide')});
+
 }
 
-function displayImage(){
+function displayImage(event){
+	console.log($(event.currentTarget).attr("imgData"));
+
 	//find the url of the image by grabbing the background-image source, store it in a variable
 	//grab the direct url of the image by getting rid of the other pieces you don't need
 
@@ -56,11 +74,15 @@ function displayImage(){
 	//change the modal-title text to the name you found above
 	//change the src of the image in the modal to the url of the image that was clicked on
 
-	//show the modal with JS.  Check for more info here: 
+	//show the modal with JS.  Check for more info here:
 	//https://www.w3schools.com/bootstrap/bootstrap_ref_js_modal.asp
+	var clickedImage = $(this).attr('imgData');
+
+	var pathlessImg = clickedImage.slice(clickedImage.indexOf('/')+1,clickedImage.lastIndexOf('.'))
+	$('.modal-title').text(pathlessImg);
+	$('.modal-body > img').attr('src',clickedImage);
+	console.log(clickedImage + " was clicked" + pathlessImg)
+	$('#galleryModal').modal();
+
+
 }
-
-
-
-
-
